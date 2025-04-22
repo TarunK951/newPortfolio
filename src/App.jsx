1c9d7085dfa1330fb4c0ab186c6d146fb539f9a5
Tarun <119518924+TarunK951@users.noreply.github.com>
2025-04-22 15:28:13 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Page1 from "./myComponents/Page1";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Page1 />
+    </div>
+  );
+}
+
+export default App;
