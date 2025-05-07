d503e515c41b74fe9a4f0910f83a166b356ef439
Tarun <119518924+TarunK951@users.noreply.github.com>
2025-05-07 12:26:58 +0530

    contact creation

---
+++ src/myComponents/Contact.jsx
@@ -0,0 +1,12 @@
+import "contact.css";
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div className="contact"></div>
+    </div>
+  );
+}
+
+export default Contact;
