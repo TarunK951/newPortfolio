f9bd5d3e473a21c5cf29bd9f5e0f4d864af5851f
Tarun <satyatarun.951@gmail.com>
2025-05-13 15:07:20 +0530

    Update Skills.jsx

--- src/myComponents/Skills.jsx
+++ src/myComponents/Skills.jsx
@@ -10,6 +10,8 @@ import {
   FaNodeJs,
   FaPython,
   FaReact,
+  SiNextdotjs
+  
 } from "react-icons/fa";
 import "./skill.css";
 
@@ -52,12 +54,14 @@ const Skills = () => {
     { icon: <FaCss3Alt />, name: "CSS3", proficiency: "Advanced" },
     { icon: <FaJs />, name: "JavaScript", proficiency: "Expert" },
     { icon: <FaReact />, name: "React", proficiency: "Expert" },
+     {icon: <SiNextdotjs />,name:"Next Js,proficiency:"Expert"}
     { icon: <FaNodeJs />, name: "Node.js", proficiency: "Intermediate" },
     { icon: <FaGitAlt />, name: "Git", proficiency: "Advanced" },
     { icon: <FaDatabase />, name: "MongoDB", proficiency: "Intermediate" },
     { icon: <FaFigma />, name: "Figma", proficiency: "Intermediate" },
     { icon: <FaJava />, name: "Java", proficiency: "Intermediate" },
     { icon: <FaPython />, name: "Python", proficiency: "Intermediate" },
+    
   ];
 
   return (
